34890e06184ddd4505513484b0e884858e6451ac
Matteo Jacopo Schembri <matteojacopo.schembri@gmail.com>
2024-10-15 09:52:38 +0200

    feat: complete playfair cipher logic

--- playfair.py
+++ playfair.py
@@ -29,3 +29,39 @@ print(key)
 
 key_matrix = np.array(list(key)).reshape(5, 5)
 print("key_matrix: \n", key_matrix)
+
+
+input_plaintext = input("\nInsert text: ").upper()
+encrypted_text = ""
+
+for idx in range(0, len(input_plaintext), 2):
+    i = input_plaintext[idx]
+    j = input_plaintext[idx + 1]
+
+    # find axis
+
+    i_x, i_y = np.where(key_matrix == i)
+    j_x, j_y = np.where(key_matrix == j)
+
+    i_x, i_y = i_x[0], i_y[0]
+    j_x, j_y = j_x[0], j_y[0]
+
+    if i_x == j_x:
+        # if same row shift colum
+        i_y = (i_y + 1) % 5
+        j_y = (j_y + 1) % 5
+    elif i_y == j_y:
+        # shift row
+        i_x = (i_x + 1) % 5
+        j_x = (j_x + 1) % 5
+
+    else:
+        # colum switch
+        i_y, j_y = j_y, i_y
+
+    i_w = key_matrix[i_x, i_y]
+    j_w = key_matrix[j_x, j_y]
+
+    encrypted_text += i_w + j_w
+
+print(encrypted_text)
